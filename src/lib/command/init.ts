c3d813fbd06e1c31e73beead51f0b6e05ea55ed7
AruSeito <AruSeito@gmail.com>
2020-09-20 12:19:11 +0800

    feat: ✨ 自动安装项目依赖文件

--- src/lib/command/init.ts
+++ src/lib/command/init.ts
@@ -1,7 +1,15 @@
-import { log, cloneRepo } from '../utils/index';
+import { log, cloneRepo, installPackage } from '../utils/index';
 
 export const init = async (name: string) => {
   log.success(`🚀创建项目:${name}`);
 
-  await cloneRepo('github:AruSeito/koa-ts-server-template', name);
+  try {
+    //clone远程模板
+    await cloneRepo('github:AruSeito/koa-ts-server-template', name);
+
+    //安装依赖
+    await installPackage(name);
+  } catch (e) {
+    console.log(e);
+  }
 };
--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -1,11 +1,16 @@
 import chalk = require('chalk');
 import ora = require('ora');
-import childProcess = require('child_process');
-import { promisify } from 'util';
+import { exec } from 'child_process';
 import * as fs from 'fs';
 import * as download from 'download-git-repo';
 import * as path from 'path';
 
+interface ExecException extends Error {
+  cmd?: string;
+  killed?: boolean;
+  code?: number;
+  signal?: NodeJS.Signals;
+}
 export const log = {
   warning(msg = '') {
     console.log(chalk.yellow(`${msg}`));
@@ -18,30 +23,53 @@ export const log = {
   },
 };
 
+/**
+ * @description clone repo
+ * @param repo 请求的git地址
+ * @param dest 目录名称
+ */
 export const cloneRepo = async (repo: string, dest: string) => {
-  const process = ora();
-  try {
-    process.start(`模板下载......${repo}`);
-    await promisify(download(repo, dest));
-  } catch (e) {
-    process.stop();
-  } finally {
-    process.succeed('模板下载成功');
-  }
-};
-
-export const runCmd = (cmd: string) => {
-  return new Promise((resolve, reject) => {
-    childProcess.exec(cmd, (err) => {
-      if (err) return reject(err);
-      return resolve();
+  return new Promise(async (resolve, reject) => {
+    const spinner = ora(`正在从 ${repo} 拉取远程模板...`).start();
+    download(repo, dest, { clone: true }, async (err: string) => {
+      if (err) {
+        spinner.color = 'red';
+        spinner.fail(chalk.red('拉取远程模板仓库失败！'));
+        return reject(err);
+      }
+      spinner.color = 'green';
+      spinner.succeed(`${chalk.grey('拉取远程模板仓库成功！')}`);
+      resolve();
     });
   });
 };
-
-export const getPackage: (field: string) => string = (field: string) => {
+/**
+ * @description 获取package.json中某字段的值
+ * @param field 字段名
+ */
+export const getPackage = (field: string) => {
   const packagePath = path.resolve(__dirname, '../../../package.json');
   const packageInfoJson = fs.readFileSync(packagePath, 'utf-8');
   const packageInfo = JSON.parse(packageInfoJson);
   return packageInfo[field];
+};
+
+export const installPackage = async (dest: string) => {
+  const installSpinner = ora('正在安装项目依赖文件，请稍后...').start();
+  return new Promise((resolve, reject) => {
+    exec(`cd ${dest} && npm install `, (error, stdout, stderr) => {
+      if (error) {
+        installSpinner.color = 'red';
+        installSpinner.fail(chalk.red('安装项目依赖失败，请自行重新安装！'));
+        reject(error);
+      } else {
+        installSpinner.color = 'green';
+        installSpinner.succeed('安装成功');
+        console.log(`${stderr}${stdout}`);
+        console.log(chalk.green(`创建项目 ${chalk.green.bold(dest)} 成功！`));
+        console.log(chalk.green(`请进入项目目录 ${chalk.green.bold(dest)} 开始工作吧！😝`));
+        resolve();
+      }
+    });
+  });
 };
